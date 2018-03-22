b18c9e8df2a2047a30c5097070afe10bf9b3d33e
Tocsin614 <paul.borrows@yahoo.com>
2018-03-22 18:02:12 -0400

    ughhh

---
+++ NocturneExpress.lua
@@ -0,0 +1,374 @@
+
+--
+--                                                                                                     .Y..                                 
+--                                                                                 .:vXgdBBQKgs.     XQq.i:vJ                               
+--                                                                          ..KRBBBBBBBBBEX1BBBD1.:1BBP:7:7YQ2X.                            
+--                                                                    iY7BBBQBQBBBBBdr     5QL..rBQBB   7Md1QgQ:                            
+--                                                                 YgBBQBQBQBQBBu::         K    UBBR   7UPQB.B:                            
+--                                                           rYRQBQBQBMBQBBBi77.            M    PBBQ   . iBL77.                            
+--                                                      .iMBBQQQBQBQBQBQQgi       i:.       BuJSBBBBB  ... .i .r                            
+--                                                :bv:rZQBQBQBgBQBPBQE:         qBQBQB:.iUuU.7iSr7QqrrKBBBQKvJ5ggB                          
+--                                      .:. .vZQBQBBBRBQBQqiBQBQr.      rIqY:  Sq.XBBQDPI:  .ui  .r PQBBBQBBBQBQQ:                          
+--                               .::.rPJqdgdEBQBQBZBPgBBQBQBRP:      rYUQBBBQB:Q:.:jr.     .dU   P  :siIBBBBQBQLB.                          
+--                      .     .KSIBQMQJBgsiUBBQDBMQdDQBQBQQs.       :q    IBBgKii:JL       ig   2v :U5: .QBgB2gEBB.                         
+--                   rBBQgI7riULiQK7MZ7BQS7BMBZQRBQBQBQZ:    :LdQBBQR  :77ri..   :d        75   E  s7I   i jIB.BIBD                         
+--            7:IJu5BQB7iUgXIiJrgLsRrvBddMBBBQBQBQBPY.      Qi  vQBQBiuuY:.      P5       .vI   Z  i71  g   uQiZvQB                         
+--       . ..QZdSiIBEP1rr1Mq.1vYYdq.rD52dP5BQ5YSir         P.  :7dji.:.          d7        7s   Q  YIJ .B   iB:ELBQ                         
+--      rrJK1s7uJj571IQE vBviQ:UMvB.UQBBB2d.         .::7r:R2YqYZ                U7        iY   Dr .1iDPBQi.2B BYQB                         
+--    .B sq::7.Yi5 ..IBBvvQgdb5PDggBQDbLiX     ..IRIrPPMbPqP:  j.                Su ::irr1Y:U    B  i17vMBQBB.2BqBP                         
+--    QdJgv.:.rr17 5i7Q2B:sIDr7E.Ysru7r:    .jRRP:.idZr.  U   :K          i..:vr1SB:17I7KuugQL   .QiQ1i:ii:rvqBuBQ                          
+--    QSgQqi::77YQ.L:Ug:.QSMggEXrIUr..  .L2QQB7: iqj     2.   v:   .::vviqQUJJ5uIvd5:vXPbgXv5B:   .IKXRP7EQBBBIZQQrEX                       
+--    .rirKZKJB:KgErrLQBEPj1dPQPJ.:    U:  L5   Lq.      P    Ld7S7JjSP2qPEXUvri7JsKEQBb.E.s::B.    :2QBBQBQYiMQBQBQi                       
+--   vr.j1Ud.rQuu5EBJJ25DB7:          QY.irU.JQ q:     ::s:YvPYQPduSrr727vs1KPEBQBQBQi7v : vBr:BX   .u:LgBEREBQBBBQB                        
+--    iijDU  .X1BrU2PIRUi       . ..:.BUQ.B :gBiS...7rYsR7Z7MBjrIIgZgdQRQQBggqBQBQBQ:  1   :BQE BQv     7qZvbMBBRXBZr:                      
+--        .     .i        .UQBQBZQurrBQ K Qi YQjQKZDEDEUQBQgvviBQBBQ7BQBBBQBQBQPJL:. UBBM1srBBQB LBBPu:i:77UQBuBBi:Qd                       
+--                     :PQBQ.IP      YB E Ki:iBiKSBBBRBddvX: 71Qu7B LQMdBDQQBr:     YrX.vbBQjQBBB::QBQBQBQBQi rBBQ2iBQ.                     
+--                 :SQQQL5i. r   .:ii uii:.UsSQBQM:  :       .QB EQ QB     UBr      B : rBQu5BQBBBX..  .BBQB .QBQBQd:Bq                     
+--             .vvBsb7irq.  BvrYJv7:7..D   Pj QQBr  .J7qEDBdr rQ.L. s   iZr BQ      P.7  1BuBQBBBQBQK7  BBBQ  BBBQBQBDSv..LYi               
+--          :uJriJrr:Y iQ  .dr..    .v Dr. :2 .L.JPBBBQPLgQBBBiUr:.i:rQY:BQ BB       BQ:  LBLXqKSXvIK ..1Li::dQBQBBQ.iJMB: :B               
+--      v5J7Bqrq7Q25SQ7 B.  Prv:irir:i IBBBvi:i  .BQBg.iQBBZBMBgBIBQBQP: gv BQ     .ruZXs..                iBQBBBQB2  rvJis.BLriii.         
+-- r7qdUQBYXP2QB qvr::  Q:  UK. .. .i7 r:gQ17S. :7BBQ riILrs::   .rBBBY  gd QB     .DdZbvgDBQ. rKBBUMQZBgb1QQi.v2  ..j7K:iv7:vrriPBE        
+-- Ri7Y2 RS .:   u:     Q:  j:. .   .  ir7BPiQ.XY1r:. i:   7s .:   UuUi  QB:7B.     7vqv 1BQBQBBqLXQi    7i       .ri   EL        BQ        
+-- Qr7ur:7r:7i.7:gR7sXBQ1BEPg:v2.:ZL.:I  B: ruB..  :        ...7. :BBBQ .Q   B      vL  :BBBQBQB:  P     gg ZYE   :i 1Q7  SPE7vv.:QU        
+--:MQSBEgQuKZ7BQBqIS77BQK7vQB vIRYgBBB51grE. .7 B i:v         :r.i:vS.i.:.:r:Qi     ri.  gBBBQBB:  7     .Kii.:  i qBKd Y iQrPBQ.           
+--budQiibU7777.ir7iqIqYbQ7iJX2 vJqM.Q7 RXSI: 1Q 7 :.5         gr     :75117s: YBb7v1J:ii.B 7QBBBQBBBQBBB     : i . BB d1L :i .BP            
+--PSvZ55BJBRQiMbsI.v57vv:r2Ssqd5rUDiQ77BBBQQYBBZBE:...  r...  L r        :..: 5EBgBQRBgi j  YQQQBQBQBBBvQQ2..sB5Q2K.:i.qBQDQB:              
+-- .  .775sK7gQZP12PusE2.L.QvrsPKviiru7XLLrLX2LdZQBdv. :.. .            .:   :Qv.BQBBQ.SQg5BBBQZQQQRDB  BBi  L2Qid:K L.::uBBQBQY            
+--          1Kr:7vLDZ7PI1XuRY 7ivgX7sBrviiSbr...:.BKI:ru. . 7   .i   .. i:. rDBQ YQi   QBBBZMQP BZi:BQ  BQ.  Q QdiR.b:7.B7.UBQBQs           
+--                 r vM7rrSErJvuSbIBYKPr:rbiY1Js.g7Prs2Y. ..7rr2i.::i::.r7:.iBQBq... vQBgBQBgQQ DB.Pr  7QBJ7rgs Qb7B.M2ui:d  BgB:           
+--                   :.irIP.1Quqi7v5:XDP5 BvB7iQDBiBUZvQjSX1..  17Y.:vBdL:r:::.irirsdQqSBQBBBgBQ.r:   iQBQi7Lri  . .           Pv..         
+--                     : iB.su.D:: K.M Yq.UiQJ.rrr.D.5rrQKbBQY:1jIQSrLvY   iv1YQDQbii:... .. .:v7..JJBQBQQ SuKXRdXqdgBDQZQUdDg5BSuJSIXIUrvJ:
+--                        .  i :iP5 Bur7E:jiUbr7gXdiiri7di.gPjQvg1:SIQBQEXBQsPPIIL.   :r2rvPP7vv5PqXSi:::.SZQQBQBQBBBQBQBQBQBQBq5BBBBQBBBs  
+--                                : . .i:D r.7:QYiS.ibIvBiYdrBURii7siKBrjb2B.SBZPUPdsRBjBEvBQQQs..   ri7vKv7.::.:i::.::i:vv1:L1dQBBgL:iqZ57 
+--                                        i  7 : .B2:1bLsrr:Pj Z:X5:v i5udQ:.gBr7JIgBQ.v7XJIiDXU. J1KSBBM1BQqBBMD:777v5i:i:.i:ir7rr.        
+--                                                 ...i Yu  Pii1gv2u7ZP:QUBiPBMXZ.PBY5KggSiSiJiBQBXrSrqB5X.LRPur.:.77EMBQBQBBBZKi:r:        
+--                                                          :.     i: u.vi:LJi.r:7.i: :IS:SrB:J..Bi IQ.d:5UrBBr7BQBgB7jr2vB:2:              
+--                                                                                                    .  v   :. . .                          
+----New series of simple scripts that just work..nothing fancy 
+-- 
+if myHero.charName ~= "Nocturne" then return end
+require "DamageLib"
+if FileExist(COMMON_PATH .. "TPred.lua") then
+	require 'TPred'
+	PrintChat("TPred library loaded")
+end
+keybindings = { [ITEM_1] = HK_ITEM_1, [ITEM_2] = HK_ITEM_2, [ITEM_3] = HK_ITEM_3, [ITEM_4] = HK_ITEM_4, [ITEM_5] = HK_ITEM_5, [ITEM_6] = HK_ITEM_6}
+local castSpell = {state = 0, tick = GetTickCount(), casting = GetTickCount() - 1000, mouse = mousePos}
+local barHeight = 8
+local barWidth = 103
+local barXOffset = 0
+local barYOffset = 0
+function SetMovement(bool)
+	if _G.EOWLoaded then
+		EOW:SetMovements(bool)
+		EOW:SetAttacks(bool)
+	elseif _G.SDK then
+		_G.SDK.Orbwalker:SetMovement(bool)
+		_G.SDK.Orbwalker:SetAttack(bool)
+	else
+		GOS.BlockMovement = not bool
+		GOS.BlockAttack = not bool
+	end
+	if bool then
+		castSpell.state = 0
+	end
+end
+
+class "Nocturne"
+local Scriptname,Version,Author,LVersion = "NocturneExpress","v1.0","Tocsin","8.6"
+
+function CurrentTarget(range)
+	if _G.SDK then
+		return _G.SDK.TargetSelector:GetTarget(range, _G.SDK.DAMAGE_TYPE_PHYSICAL);
+	elseif _G.EOW then
+		return _G.EOW:GetTarget(range)
+	else
+		return _G.GOS:GetTarget(range,"AD")
+	end
+end
+
+function Nocturne:__init()
+	
+	self:LoadSpells()
+	self:LoadMenu()
+	Callback.Add("Tick", function() self:Tick() end)
+	Callback.Add("Draw", function() self:Draw() end)
+	local orbwalkername = ""
+	if _G.SDK then
+		orbwalkername = "IC'S orbwalker"		
+	elseif _G.EOW then
+		orbwalkername = "EOW"	
+	elseif _G.GOS then
+		orbwalkername = "Noddy orbwalker"
+	else
+		orbwalkername = "Orbwalker not found"
+	end
+	PrintChat(Scriptname.." "..Version.." - Loaded...."..orbwalkername)
+end
+
+function Nocturne:LoadSpells()
+	Q = {Range = 1200, Width = 80, Delay = 0.25, Speed = 1600, Collision = false, aoe = true, Type = "line"}
+	W = {}
+	E = {Range = 425}
+	R = {Range = 2500}
+end
+
+function Nocturne:LoadMenu()
+	self.Menu = MenuElement({type = MENU, id = "NocturneExpress", name = Scriptname})
+	self.Menu:MenuElement({id = "ComboMode", name = "Combo", type = MENU})
+	self.Menu.ComboMode:MenuElement({id = "UseQ", name = "Q: Duskbringer", value = true})
+	self.Menu.ComboMode:MenuElement({id = "UseE", name = "E: Unspeakable Horror", value = true})
+	self.Menu.ComboMode:MenuElement({id = "UseHYDRA", name = "Use hydra", value = true})	
+	self.Menu:MenuElement({id = "HarassMode", name = "Harass", type = MENU})
+	self.Menu.HarassMode:MenuElement({id = "UseQ", name = "Q: Duskbringer", value = true})
+	self.Menu.HarassMode:MenuElement({id = "UseE", name = "E: Unspeakable Horror", value = true})
+	self.Menu.HarassMode:MenuElement({id = "harassActive", name = "Harass key", key = string.byte("C")})
+	self.Menu:MenuElement({id = "ClearMode", name = "Clear", type = MENU})
+	self.Menu.ClearMode:MenuElement({id = "UseQ", name = "Q: Duskbringer", value = true})
+	self.Menu.ClearMode:MenuElement({id = "UseE", name = "E: Unspeakable Horror", value = true})
+	self.Menu.ClearMode:MenuElement({id = "clearActive", name = "Clear key", key = string.byte("V")})
+	self.Menu:MenuElement({id = "CustomSpellCast", name = "Use custom spellcast", tooltip = "wtf problems with wrong directions", value = true})
+	self.Menu:MenuElement({id = "delay", name = "Custom spellcast delay", value = 100, min = 0, max = 200, step = 5,tooltip = "increase this one if spells is going completely wrong direction", identifier = ""})
+	self.Menu:MenuElement({id = "blank", type = SPACE , name = ""})
+	self.Menu:MenuElement({id = "blank", type = SPACE , name = "Script Ver: "..Version.. " - LoL Ver: "..LVersion.. ""})
+	self.Menu:MenuElement({id = "blank", type = SPACE , name = "by "..Author.. ""})
+end
+
+function GetInventorySlotItem(itemID)
+		assert(type(itemID) == "number", "GetInventorySlotItem: wrong argument types (<number> expected)")
+		for _, j in pairs({ ITEM_1, ITEM_2, ITEM_3, ITEM_4, ITEM_5, ITEM_6}) do
+			if myHero:GetItemData(j).itemID == itemID and myHero:GetSpellData(j).currentCd == 0 then return j end
+		end
+		return nil
+	    end
+	
+	function UseHydra()
+		local HTarget = CurrentTarget(125)
+		if HTarget then 
+			local hydraitem = GetInventorySlotItem(3748) or GetInventorySlotItem(3077)
+			if hydraitem and myHero.attackData.state == STATE_WINDDOWN then
+				Control.CastSpell(keybindings[hydraitem],HTarget.pos)
+                Control.Attack(HTarget)
+			end
+		end
+	end
+    function UseHydraminion()
+        for i = 1, Game.MinionCount() do
+	    local minion = Game.Minion(i)
+        if minion and minion.team == 300 or minion.team ~= myHero.team then 
+			local hydraitem = GetInventorySlotItem(3748) or GetInventorySlotItem(3077)
+			if hydraitem and myHero.attackData.state == STATE_WINDDOWN then
+				Control.CastSpell(keybindings[hydraitem])
+                Control.Attack(minion)
+			end
+		end
+        end
+	end
+
+    function UseBotrk()
+		local BTarget = CurrentTarget(400)
+		if BTarget then 
+			local botrkitem = GetInventorySlotItem(3153) or GetInventorySlotItem(3144)
+			if botrkitem then
+				Control.CastSpell(keybindings[botrkitem],BTarget.pos)
+			end
+		end
+	end
+
+
+function IsRecalling()
+	for K, Buff in pairs(GetBuffs(myHero)) do
+		if Buff.name == "recall" and Buff.duration > 0 then
+			return true
+		end
+	end
+	return false
+end
+
+function ValidTarget(target, range)
+	range = range and range or math.huge
+	return target ~= nil and target.valid and target.visible and not target.dead and target.distance <= range
+end
+
+function Nocturne:Tick()
+    if myHero.dead or Game.IsChatOpen() == true or IsRecalling() == true then return end
+	if self.Menu.HarassMode.harassActive:Value() and self:EnemyInRange(1000) then
+		self:Harass()
+	end
+	if self.Menu.ComboMode.comboActive:Value() and self:EnemyInRange(1000) then
+		self:Combo()
+	end
+	if self.Menu.ClearMode.clearActive:Value() then
+		self:Jungle()
+	end
+end
+
+function Nocturne:HasBuff(unit, buffname)
+	for i = 0, unit.buffCount do
+		local buff = unit:GetBuff(i)
+		if buff.name == buffname and buff.count > 0 then 
+			return true
+		end
+	end
+	return false
+end
+
+function Nocturne:IsReady(spell)
+	return Game.CanUseSpell(spell) == 0
+end
+
+function Nocturne:CheckMana(spellSlot)
+	return myHero:GetSpellData(spellSlot).mana < myHero.mana
+end
+
+function Nocturne:CanCast(spellSlot)
+	return self:IsReady(spellSlot)
+end
+
+function EnableMovement()
+	SetMovement(true)
+end
+
+function ReturnCursor(pos)
+	Control.SetCursorPos(pos)
+	DelayAction(EnableMovement,0.1)
+end
+
+function LeftClick(pos)
+	Control.mouse_event(MOUSEEVENTF_LEFTDOWN)
+	Control.mouse_event(MOUSEEVENTF_LEFTUP)
+	DelayAction(ReturnCursor,0.05,{pos})
+end
+function Nocturne:Draw()
+    local textPos = myHero.pos:To2D()
+    if self:CanCast(_Q) then Draw.Circle(myHero.pos, 650, 3,  Draw.Color(255, 000, 222, 255)) end
+end
+
+function Nocturne:CastSpell(spell,pos)
+	local customcast = self.Menu.CustomSpellCast:Value()
+	if not customcast then
+		Control.CastSpell(spell, pos)
+		return
+	else
+		local delay = self.Menu.delay:Value()
+		local ticker = GetTickCount()
+		if castSpell.state == 0 and ticker > castSpell.casting then
+			castSpell.state = 1
+			castSpell.mouse = mousePos
+			castSpell.tick = ticker
+			if ticker - castSpell.tick < Game.Latency() then
+				SetMovement(false)
+				Control.SetCursorPos(pos)
+				Control.KeyDown(spell)
+				Control.KeyUp(spell)
+				DelayAction(LeftClick,delay/1000,{castSpell.mouse})
+				castSpell.casting = ticker + 500
+			end
+		end
+	end
+end
+
+function Nocturne:Combo()
+	if (not _G.SDK and not _G.GOS) then return end
+	local target = target or (_G.SDK and _G.SDK.TargetSelector:GetTarget(1300, _G.SDK.DAMAGE_TYPE_PHYSICAL)) or (_G.GOS and _G.GOS:GetTarget(E.Range,"AD"))
+	if target and target.type == "AIHeroClient" then
+    if self.Menu.ComboMode.UseHYDRA:Value() and target.pos:DistanceTo(myHero.pos) < 150 then
+        if myHero.attackData.state == STATE_WINDDOWN and not self:CanCast(_Q) then
+            UseHydra()
+        end
+    end
+
+    if self:CanCast(_E) and target.pos:DistanceTo(myHero.pos) < E.Range and self.Menu.ComboMode.UseE:Value() then 
+			self:CastSpell(HK_E,target)
+	end
+
+	if self:CanCast(_Q) and target.pos:DistanceTo(myHero.pos) < Q.Range and self.Menu.ComboMode.UseQ:Value() then           
+        local castpos,HitChance, pos = TPred:GetBestCastPosition(target, Q.Delay , Q.Width, Q.Range,Q.Speed, myHero.pos, Q.ignorecol, Q.Type )
+		if (HitChance > 0 ) then
+			self:CastSpell(HK_Q,castpos)
+		end
+	end
+
+    if self:EnemyInRange(400) and not self:CanCast(_Q) then 
+        local BTarget = CurrentTarget(400)
+        if BTarget then
+            if myHero.pos:DistanceTo(BTarget.pos) < 400 then
+			    UseBotrk()
+            end
+        end
+    end
+	end
+end
+
+function Nocturne:GetEnemyHeroes()
+	self.EnemyHeroes = {}
+	for i = 1, Game.HeroCount() do
+		local Hero = Game.Hero(i)
+		if Hero.isEnemy then
+			table.insert(self.EnemyHeroes, Hero)
+		end
+	end
+	return self.EnemyHeroes
+end
+
+function Nocturne:EnemyInRange(range)
+	local count = 0
+	for i, target in ipairs(self:GetEnemyHeroes()) do
+		if target.pos:DistanceTo(myHero.pos) < range then 
+			count = count + 1
+		end
+	end
+	return count
+end
+
+function Nocturne:Harass()
+	if (not _G.SDK and not _G.GOS) then return end
+	local target = target or (_G.SDK and _G.SDK.TargetSelector:GetTarget(1300, _G.SDK.DAMAGE_TYPE_PHYSICAL)) or (_G.GOS and _G.GOS:GetTarget(E.Range,"AD"))
+	if target and target.type == "AIHeroClient" then
+    if self.Menu.ComboMode.UseHYDRA:Value() and target.pos:DistanceTo(myHero.pos) < 150 then
+        if myHero.attackData.state == STATE_WINDDOWN and not self:CanCast(_Q) then
+            UseHydra()
+        end
+    end
+
+    if self:CanCast(_E) and target.pos:DistanceTo(myHero.pos) < E.Range and self.Menu.HarassMode.UseE:Value() then  
+		self:CastSpell(HK_E,target)
+	end
+
+	if self:CanCast(_Q) and target.pos:DistanceTo(myHero.pos) < Q.Range and self.Menu.HarassMode.UseQ:Value() then           
+        local castpos,HitChance, pos = TPred:GetBestCastPosition(target, Q.Delay , Q.Width, Q.Range,Q.Speed, myHero.pos, Q.ignorecol, Q.Type )
+		if (HitChance > 0 ) then
+			self:CastSpell(HK_Q,castpos)
+		end
+	end
+	end
+end
+
+function Nocturne:Jungle()
+	for i = 1, Game.MinionCount() do
+	local minion = Game.Minion(i)
+    if minion and minion.team == 300 or minion.team ~= myHero.team then
+        if self.Menu.ComboMode.UseHYDRA:Value() and minion then
+            if myHero.attackData.state == STATE_WINDDOWN and not self:CanCast(_Q) then
+                UseHydraminion()
+            end
+        end
+    
+        if self:CanCast(_E) and myHero.pos:DistanceTo(minion.pos) < 500 then 
+            if self.Menu.ClearMode.UseE:Value() and minion then
+                self:CastSpell(HK_E,minion)
+            end
+        end
+    
+        if self:CanCast(_Q) and myHero.pos:DistanceTo(minion.pos) < 500 then 
+            if self.Menu.ClearMode.UseQ:Value() and minion then           
+                self:CastSpell(HK_Q,minion)
+            end
+        end
+	end
+	end
+end
+
+function OnLoad()
+	Nocturne()
+end
